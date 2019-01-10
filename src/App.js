d5d67920634ffc26e58cbaa87ccb9e3794c0890e
Tusho <armin.kuhestani@gmail.com>
2019-01-10 22:28:03 +1100

    Passed down books props and mapped through books

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class BooksApp extends React.Component {
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({books})
-      console.log({books})
     })
   }
 
@@ -24,15 +23,10 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+                {
 
+                }
+                <input type="text" placeholder="Search by title or author"/>
               </div>
             </div>
             <div className="search-books-results">
@@ -45,9 +39,9 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <BookShelf title="Currently Reading"/>
-              <BookShelf title="Want to Read"/>
-              <BookShelf title="Read"/>
+              <BookShelf title="Currently Reading" books={this.state.books}/>
+              <BookShelf title="Want to Read" books={this.state.books}/>
+              <BookShelf title="Read" books={this.state.books}/>
             </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
--- src/bookshelf.js
+++ src/bookshelf.js
@@ -6,16 +6,12 @@ class BookShelf extends Component {
 
 
   render() {
-    const books= this.props.books;
+        console.log(this.props);
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <Books />
-            </li>
-          </ol>
+              <Books books={this.props.books}/>
         </div>
       </div>
     )
