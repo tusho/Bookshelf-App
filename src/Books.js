4f520885202c9120c141767fafc1f5375cdedd03
Tusho <armin.kuhestani@gmail.com>
2019-01-16 21:46:38 +1100

    Added event handler to the book component

--- src/Books.js
+++ src/Books.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 class Books extends Component {
+
   render() {
     const books = this.props.books;
     const shelf = this.props.shelf.toLowerCase().replace(/\s/g, '');
-    let selectedBooks = books.filter(book => shelf == book.shelf.toLowerCase())
+    let selectedBooks = books.filter(book => shelf === book.shelf.toLowerCase())
+
+    let handleClick = (e) => {
+      let updateShelf = e.target.value.toLowerCase();
+      console.log({updateShelf});
+    }
+
     console.log(selectedBooks);
     return (
       <ol className="books-grid">
@@ -14,7 +21,7 @@ class Books extends Component {
                 <div className="book-top">
                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}})` }}></div>
                   <div className="book-shelf-changer">
-                    <select>
+                    <select value={book.shelf} onClick={handleClick} onChange={(event) => console.log(event)}>
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
